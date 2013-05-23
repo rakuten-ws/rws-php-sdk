03f116dc1273a3a5fd9d993aeee16de0e1ad30f5
Shogo Kawahara <shogo.a.kawahara@mail.rakuten.com>
2013-05-23 20:47:47 +0900

    Add new Books API version definition

--- lib/RakutenRws/Api/Definition/BooksDVDSearch2.php
+++ lib/RakutenRws/Api/Definition/BooksDVDSearch2.php
@@ -21,6 +21,7 @@ class RakutenRws_Api_Definition_BooksDVDSearch2 extends RakutenRws_Api_AppRakute
         $autoSetIterator = true,
         $isRequiredAccessToken = false,
         $versionMap = array(
+            '2013-05-22' => '20130522',
             '2012-11-28' => '20121128'
         );
 
--- lib/RakutenRws/Api/Definition/BooksSoftwareSearch2.php
+++ lib/RakutenRws/Api/Definition/BooksSoftwareSearch2.php
@@ -21,6 +21,7 @@ class RakutenRws_Api_Definition_BooksSoftwareSearch2 extends RakutenRws_Api_AppR
         $autoSetIterator = true,
         $isRequiredAccessToken = false,
         $versionMap = array(
+            '2013-05-22' => '20130522',
             '2012-11-28' => '20121128'
         );
 
